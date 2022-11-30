00a64670a1fa0ac3011320c3491ec274b8588ced
Boriss Varlamovs <bvarlamo@c2s12d63.42wolfsburg.de>
2022-11-30 10:57:31 +0100

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bvarlamo <bvarlamo@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/02 09:54:36 by bvarlamo          #+#    #+#             */
+/*   Updated: 2021/09/22 09:16:17 by bvarlamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+
+#  define BUFFER_SIZE 42
+# endif
+
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *str, int c);
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s1);
+
+#endif
